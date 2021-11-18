511c89dd6e2b99acd68fad4c2bc8a95fa85ce856
Gal Gir <ggir@mail.yu.edu>
2021-11-18 14:48:40 -0500

    express app setup

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 var morgan = require('morgan');
 const layout = require('./views/layout')
 const public = express.static('./public');
+const { db } = require('./models');
+
 //app.use(public);
 
 // parses url-encoded bodies
@@ -11,9 +13,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(public);
 
 // parses json bodies
-//app.use(express.json());
+app.use(express.json());
+
+db.authenticate()
+  .then(() => {
+    console.log('connected to the database');
+  })
 
 app.get('/', (req, res)=> {
-    res.send('happy birthday');
+    //res.send('happy birthday');
     res.send(layout(''))
 })
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`App listening in port ${PORT}`);
+});
